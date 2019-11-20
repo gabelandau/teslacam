cb117d2f22eab1c4a03887dd752feec614e15683
Gabe Landau <glandau@constantcontact.com>
2019-11-20 13:09:51 -0500

    Initial commit

---
+++ src/components/FileBrowser.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="file is-boxed">
+    <label class="file-label">
+      <input class="file-input" type="file" ref="uploaded_folder" @change="folderUploaded" webkitdirectory directory multiple>
+      <span class="file-cta">
+        <span class="file-icon">
+          <i class="fas fa-upload"></i>
+        </span>
+        <span class="file-label">
+          Choose a folder...
+        </span>
+      </span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileBrowser',
+  methods: {
+    folderUploaded: function () {
+      console.log(this.$refs.uploaded_folder.files)
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <FileBrowser />
+  </div>
+</template>
+
+<script>
+import FileBrowser from '@/components/FileBrowser'
+
+export default {
+  name: 'home',
+  components: { FileBrowser }
+}
+</script>
